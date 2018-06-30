dd338bd23425e4d01146cf7ba5190f6941726563
dejavu8431 <40553809+dejavu8431@users.noreply.github.com>
2018-06-30 19:49:36 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,43 @@
+
 // Select color input
 // Select size input
+var height, width, color
+
+$('#sizePicker').submit(function (event){
+    event.preventDefault();
+    const height = $('#inputHeight').val();
+    const width = $('#inputWidth').val();
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+    makeGrid(height, width);
+    console.log("height is: " + height + " & width is: " + width);
+});
 
-// Your code goes here!
 
-}
+function makeGrid(x, y) {
+    $('tr').remove();
+
+    // Your code goes here!
+    for (var i = 1; i <= x; i++) {
+        $('#pixelCanvas').append('<tr id=table' + 1 + '></tr>');
+        for (var j = 1; j <= y; j++) {
+            $('table' + 1).append('<td></td>');
+        }
+    } 
+
+    // add color to cell
+    $('td').click(function addColor() {
+        color = $('#colorPicker').val();
+
+        if ($(this).attr('style')) {
+            $(this).removeAttr('style')
+        } 
+        else 
+        {
+            $(this).attr('style', 'background-color' + color);
+        }
+    }};
+
+
+
